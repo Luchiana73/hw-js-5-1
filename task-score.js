64dfc21686d0e223613d248eea5465127eff15a1
Svetlana <luchiana.sv@gmail.com>
2023-01-24 21:55:20 +0100

    added info about average score for 3 modules

---
+++ task-score.js
@@ -0,0 +1,13 @@
+let intModuleScore = 100;
+let gitModuleScore = 269;
+let jsModuleScore = 565;
+
+let totalScore = intModuleScore + gitModuleScore + jsModuleScore;
+
+let firstName = "Svetlana";
+let surname = "Gorodetskaya";
+let fullName = firstName + " " + surname
+
+console.log(`Total score for 3 modules for ${fullName} is ${totalScore}.`);
+console.log(`Average score for 3 modules for ${fullName} is ${Math.trunc(totalScore/3)}.`);
+
