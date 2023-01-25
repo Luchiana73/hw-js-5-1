4d7f9b96cf595777b36caf03ec025f308ca6e9f8
Svetlana <luchiana.sv@gmail.com>
2023-01-25 12:04:51 +0100

    added an abbreviation of the full name

--- task-score.js
+++ task-score.js
@@ -6,8 +6,12 @@ let totalScore = intModuleScore + gitModuleScore + jsModuleScore;
 
 let firstName = "Svetlana";
 let surname = "Gorodetskaya";
-let fullName = firstName + " " + surname
+let shortName = firstName.slice(0, 1);
+let fullName = shortName + "." + " " + surname;
 
 console.log(`Total score for 3 modules for ${fullName} is ${totalScore}.`);
-console.log(`Average score for 3 modules for ${fullName} is ${Math.trunc(totalScore/3)}.`);
-
+console.log(
+  `Average score for 3 modules for ${fullName} is ${Math.trunc(
+    totalScore / 3
+  )}.`
+);
